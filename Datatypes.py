e8984da6d17a28e3e162aadbada8789951d2ac23
Dhanunjaya23-02-2006 <dhanunjayaambati23@gmail.com>
2025-01-24 18:04:28 +0530

    Update Datatypes.py

--- Datatypes.py
+++ Datatypes.py
@@ -1,69 +1,12 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 7,
-   "id": "8f87fc1f-34ff-4ebd-b429-86068ce15a46",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "42\n",
-      "5.6\n",
-      "Hello\n",
-      "True\n",
-      "<class 'int'>\n",
-      "<class 'float'>\n",
-      "<class 'str'>\n",
-      "<class 'bool'>\n"
-     ]
-    }
-   ],
-   "source": [
-    "a=42\n",
-    "b=5.6\n",
-    "c=\"Hello\"\n",
-    "d=True\n",
-    "print(a)\n",
-    "print(b)\n",
-    "print(c)\n",
-    "print(d)\n",
-    "print(type(a))\n",
-    "print(type(b))\n",
-    "print(type(c))\n",
-    "print(type(d))"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "74e9d379-ac69-48c0-a079-f7da25196f6e",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.12.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+a=42
+b=5.6
+c="Hello"
+d=True
+print(a)
+print(b)
+print(c)
+print(d)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
